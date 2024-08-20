9954fade033398e1652b99e72d650c7c7ac561f0
Venktesh Badgaiyan <venkteshbadgaiyan@gmail.com>
2024-08-20 21:34:51 +0530

    day-12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/hackathon_app.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'package:flutter_application_1/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
@@ -10,16 +12,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(15, (index) => Hackathon.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Hackathon app"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
-        body:Center(
-          child:Container(
-            child: Text("Welcome to $days days of flutter with $name"),
-          ),
-        ),
-        drawer: MyDrawer(),
-      );
-  }
-}
+      ),
+      drawer: MyDrawer(),
+    ); // Closing Scaffold
+  } // Closing build method
+} // Closing HomePage class
