1ffcad408cfe6c09f1f2481d9fee9403c139e787
Venktesh Badgaiyan <venkteshbadgaiyan@gmail.com>
2024-08-23 00:37:11 +0530

    day-13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,37 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/hackathon_app.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Venktesh";
+class HomePage extends StatefulWidget {
+
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Venktesh";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    final datajson =  await rootBundle.loadString("assets/files/data.json");
+    final decodedata = jsonDecode(datajson);
+    var productdata = decodedata["products"];
+    print(productdata);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,5 +53,4 @@ class HomePage extends StatelessWidget {
       ),
       drawer: MyDrawer(),
     ); // Closing Scaffold
-  } // Closing build method
-} // Closing HomePage class
+  } } // Closing HomePage class
